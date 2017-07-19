4bed32dae95e4fd2e838592f551734b41a65c3ac
Arthur jl DUPONT <arthur.dupont@telecom-paristech.fr>
2017-07-19 19:24:10 +0200

    Update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #python 
-
 apt-get install -y build-essential checkinstall
 apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 cd ~/Downloads/
@@ -13,13 +12,11 @@ make install
 
 
 #pip
-
 apt-get install -y python-pip python-dev build-essential 
 pip install -y --upgrade pip 
 
 
 #mopidy
-
 cd /
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
@@ -27,12 +24,15 @@ apt-get update
 apt-get install -y mopidy
 
 
-# scan music
-
-mopidy local scan
-
-
 # mopidy webextension
 pip install configobj
 pip install Mopidy-Material-Webclient
 pip install Mopidy-Iris
+pip install Mopidy-Local-SQLite
+
+
+# scan music
+mopidy local scan
+
+
+
